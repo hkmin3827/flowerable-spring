59d6c8eff683db8f246e347627a36ac4d0212c57
hakyungmin <hkmin3827@gmail.com>
2026-03-03 19:16:18 +0900

    redis host name 수정

--- src/main/java/com/flowerable/spring/infra/redis/RedisConnectionTest.java
+++ src/main/java/com/flowerable/spring/infra/redis/RedisConnectionTest.java
@@ -1,20 +1,20 @@
-package com.flowerable.spring.infra.redis;
-
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class RedisConnectionTest {
-
-    private final StringRedisTemplate redisTemplate;
-
-    @PostConstruct
-    public void test() {
-        redisTemplate.opsForValue().set("hello", "redis");
-        String value = redisTemplate.opsForValue().get("hello");
-        System.out.println("REDIS TEST VALUE = " + value);
-    }
-}
+//package com.flowerable.spring.infra.redis;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class RedisConnectionTest {
+//
+//    private final StringRedisTemplate redisTemplate;
+//
+//    @PostConstruct
+//    public void test() {
+//        redisTemplate.opsForValue().set("hello", "redis");
+//        String value = redisTemplate.opsForValue().get("hello");
+//        System.out.println("REDIS TEST VALUE = " + value);
+//    }
+//}
